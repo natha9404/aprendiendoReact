9898651708aaae743ec478be08339ff4fcda42b4
Nathalia Bedoya <nathalia.bedoya@grupossi.com>
2020-10-27 17:54:38 -0500

    componentes en ReactJS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+// function Hello (props) {
+// return <h2>{props.tittle}</h2>
+// }
+
+// const Hello = (props) => <h2>{props.tittle}</h2>
+
+class Hello extends Component {
+
+  render () {
+  return <h2>{this.props.tittle}</h2>
+  }
+
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Hello tittle='Hello from props' />
         <a
           className="App-link"
           href="https://reactjs.org"
